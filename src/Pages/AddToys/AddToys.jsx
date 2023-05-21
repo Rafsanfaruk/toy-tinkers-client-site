87268b942194b0ae17f0633b748b307c526e83fd
Rafsanfaruk <39384467+Rafsanfaruk@users.noreply.github.com>
2023-05-21 10:30:21 +0600

    add toy form create

--- src/Pages/AddToys/AddToys.jsx
+++ src/Pages/AddToys/AddToys.jsx
@@ -3,16 +3,103 @@
 import { useLoaderData } from "react-router-dom";
 
 const AddToys = () => {
+  const toys = useLoaderData();
 
-    const toys =useLoaderData();
-    const {_id, picture_url, seller_name, sub_category, price, available_quantity,seller_email } =
-    toys;
+  const {
+    _id,
+    picture_url,
+    seller_name,
+    sub_category,
+    price,
+    available_quantity,
+    seller_email,
+  } = toys;
 
-    return (
-        <div>
-            <h2>Added Toy here:{toys.picture_url}</h2>
+  return (
+    <div className="m-10">
+      <form>
+        <div className=" grid grid-cols-1 md:grid-cols-2 gap-5">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Email"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Name"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Sub-category</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Category"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Price</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Price"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Rating</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Rating"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available quantity</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Available quantity"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Detail description</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Details"
+              className="input input-bordered"
+            />
+          </div>
         </div>
-    );
+        <div className="form-control mt-6">
+          <input
+            className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full"
+            type="submit"
+            value="Add toy "
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddToys;
